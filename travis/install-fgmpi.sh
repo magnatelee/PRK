7174be4f4b96e9ef14bbd30718aed98eef2980d5
Jeff Hammond <jeff.science@gmail.com>
2016-01-23 12:25:34 -0800

    try FGMPI on Mac w/ Git version since has fixes

--- travis/install-fgmpi.sh
+++ travis/install-fgmpi.sh
@@ -5,23 +5,25 @@ TRAVIS_ROOT="$1"
 
 if [ ! -d "$TRAVIS_ROOT/fgmpi" ]; then
 
+    cd $TRAVIS_ROOT
+
     # TAR build
-    wget --no-check-certificate -q http://www.cs.ubc.ca/~humaira/code/fgmpi-2.0.tar.gz
-    tar -C $TRAVIS_ROOT -xzf fgmpi-2.0.tar.gz
-    cd $TRAVIS_ROOT/fgmpi-2.0
+    #wget --no-check-certificate -q http://www.cs.ubc.ca/~humaira/code/fgmpi-2.0.tar.gz
+    #tar -C $TRAVIS_ROOT -xzf fgmpi-2.0.tar.gz
+    #cd $TRAVIS_ROOT/fgmpi-2.0
 
     # GIT build
-    #cd $TRAVIS_ROOT
-    #git clone --depth 10 https://github.com/humairakamal/fgmpi.git fgmpi-source
-    #cd fgmpi-source
-    ## this may fail on older autotools
-    #./autogen.sh
+    git clone --depth 10 https://github.com/humairakamal/fgmpi.git fgmpi-source
+    cd $TRAVIS_ROOT/fgmpi-source
+    sh ./install-autotools.sh $TRAVIS_ROOT
+    ./autogen.sh
 
     # TAR or GIT
     mkdir build && cd build
     # Clang defaults to C99, which chokes on "Set_PROC_NULL"
     # -Wno-macro-redefined silences numerous instances of the same warning.
-    ../configure --disable-fortran --disable-romio CFLAGS="-std=gnu89 -Wno-macro-redefined" --prefix=$TRAVIS_ROOT/fgmpi
+    #../configure --disable-fortran --disable-romio CFLAGS="-std=gnu89 -Wno-macro-redefined" --prefix=$TRAVIS_ROOT/fgmpi
+    ../configure --disable-fortran --disable-romio --prefix=$TRAVIS_ROOT/fgmpi
     make -j2
     make install
 
