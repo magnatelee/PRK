f34954192d5c5294280d164cd0bc8ccda627da45
Jeff Hammond <jeff.science@gmail.com>
2016-01-24 15:33:57 -0800

    need to disabled fortran

--- travis/install-fgmpi.sh
+++ travis/install-fgmpi.sh
@@ -22,7 +22,7 @@ if [ ! -d "$TRAVIS_ROOT/fgmpi" ]; then
     # Clang defaults to C99, which chokes on "Set_PROC_NULL"
     # -Wno-macro-redefined silences numerous instances of the same warning.
     #../configure --disable-fortran --disable-romio CFLAGS="-std=gnu89 -Wno-macro-redefined" --prefix=$TRAVIS_ROOT/fgmpi
-    ../configure --prefix=$TRAVIS_ROOT/fgmpi
+    ../configure --prefix=$TRAVIS_ROOT/fgmpi --disable-fortran --disable-romio
     make -j2
     make install
 
